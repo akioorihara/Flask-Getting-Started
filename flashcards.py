925d545d1dc476c553e366bbb472bec94c7e48a3
Akio <akio.orihara@outlook.com>
2020-07-08 17:56:52 -0500

    adding a few files

--- flashcards.py
+++ flashcards.py
@@ -1,21 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 from datetime import datetime
+from model import db
 
 app= Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return "welcome!"
-
+    return render_template(
+        "welcome.html", message="Here's a message from the view"
+    )
 
 @app.route("/date")
 def date():
     return "This page was served at " + str(datetime.now())
 
+
+@app.route("/card")
+def card_view():
+    card = db[0]
+    return render_template("card.html", card=card)
+
 #add how many times this page was viewed
-counter =0
-@app.route("/counter")
-def count():
-    global counter
-    counter += 1
-    return "this page was viewed :" + str(counter) + " times"
+#counter =0
+#@app.route("/counter")
+#def count():
+#    global counter
+#    counter += 1
+#    return "this page was viewed :" + str(counter) + " times"
